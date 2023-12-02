376362e5d1cf579d1f82f8215f4fa5797bcfc0a4
dessertivore <landau.sophie@gmail.com>
2023-12-02 16:04:51 +0000

    Complete Day 2

--- day-2/day_2.py
+++ day-2/day_2.py
@@ -6,8 +6,8 @@ input = open("day-2/input_file.txt", "r").readlines()
 
 def parse_input(input: list) -> dict:
     dict = {}
+    # Remove 'Game' and line breaks, then create list of all outputs in dict
     input_stripped = [(entry.strip("Game ")).strip("\n") for entry in input]
-    """Remove Game and line breaks, then create list of all outputs"""
     for line in input_stripped:
         key, outcome = line.split(": ")
         split_outcome = []
@@ -36,3 +36,28 @@ def day2(games_list: dict) -> int:
 
 
 print(day2(day2_dict))
+
+
+def d2_part2(input: dict, key: int) -> int:
+    min = {"red": 0, "green": 0, "blue": 0}
+    game_list = input[key]
+    for entry in game_list:
+        num, colour = entry.split(" ")
+        if int(num) > min[colour]:
+            min[colour] = int(num)
+        else:
+            continue
+    power = min["red"] * min["green"] * min["blue"]
+    return power
+
+
+def total_d2_part2(input: dict) -> int:
+    x = 1
+    total = 0
+    while x <= len(input):
+        total += d2_part2(input, x)
+        x += 1
+    return total
+
+
+print(total_d2_part2(day2_dict))
--- day-2/day_2_tests.py
+++ day-2/day_2_tests.py
@@ -1,7 +1,12 @@
-from day_2 import parse_input, day2
+from day_2 import parse_input, day2, d2_part2, total_d2_part2
 
 test_input = parse_input(open("day-2/input_tests.txt", "r").readlines())
 
-print(test_input)
-
 assert day2(test_input) == 8
+
+assert d2_part2(test_input, 1) == 48
+assert d2_part2(test_input, 2) == 12
+assert d2_part2(test_input, 3) == 1560
+assert d2_part2(test_input, 4) == 630
+assert d2_part2(test_input, 5) == 36
+assert total_d2_part2(test_input) == 2286
