9a5c9e89a9fe63cffab0ebc7999b17091a7b9090
dessertivore <landau.sophie@gmail.com>
2023-12-03 17:00:18 +0000

    Complete Day 3

--- day-3/day_3.py
+++ day-3/day_3.py
@@ -3,6 +3,9 @@ input1 = [line.strip("\n") for line in input]
 
 
 def populate_table(array: list[str]):
+    """
+    Create a table where each row x corresponds to 1 line in the input file.
+    """
     input_table: list = [["" for _ in range(len(array[0]))] for _ in range(len(array))]
     for x in range(len(array)):
         for y in range(len(array[0])):
@@ -14,6 +17,9 @@ input_table = populate_table(input1)
 
 
 def check_diag(y, last_y, x, array) -> bool:
+    """
+    Check all adjacent cells around number for symbols.
+    """
     num = False
     for dx in range(max(x - 1, 0), min(x + 2, len(array)), 1):
         for dy in range(max(y - 1, 0), min(last_y + 2, len(array[0])), 1):
@@ -25,6 +31,10 @@ def check_diag(y, last_y, x, array) -> bool:
 
 
 def day3(array: list) -> int:
+    """
+    Find all numbers in the input file. Check if they have a symbol in adjacent cell.
+    If they do, add them to totalsum.
+    """
     x = 0
     total_sum = 0
     while x < len(array):
@@ -51,3 +61,56 @@ def day3(array: list) -> int:
 
 
 print(day3(input_table))
+
+
+def check_ratio(x: int, y: int, array: list) -> int:
+    """
+    Check if asterisk is a gear and calculate ratio
+    """
+    num_list = []
+    ratio = 0
+    dx = max(x - 1, 0)
+    while dx < min(x + 2, len(array)):
+        dy = max(y - 1, 0)
+        while dy < min(y + 2, len(array[0])):
+            if array[dx][dy].isdigit():
+                num = ""
+                first_y = dy
+                last_y = dy
+                while first_y >= 0 and array[dx][first_y - 1].isdigit():
+                    first_y -= 1
+                while last_y < len(array[0]) - 1 and array[dx][last_y + 1].isdigit():
+                    last_y += 1
+                for z in range(first_y, last_y + 1):
+                    num += array[dx][z]
+                print(num)
+                num_list.append(num)
+                dy = last_y
+            dy += 1
+        dx += 1
+    if len(num_list) == 2:
+        print(num_list)
+        int1 = num_list[0]
+        int2 = num_list[1]
+        ratio = int(int1) * int(int2)
+    return ratio
+
+
+def day3_asterisk(array: list) -> int:
+    """
+    Find asterisks and sum ratios.
+    """
+    x = 0
+    total_ratio = 0
+    while x < len(array):
+        y = 0
+        while y < len(array[0]):
+            if array[x][y] == "*":
+                ratio: int = check_ratio(x, y, array)
+                total_ratio += ratio
+            y += 1
+        x += 1
+    return total_ratio
+
+
+print(day3_asterisk(input_table))
--- day-3/day_3_tests.py
+++ day-3/day_3_tests.py
@@ -1,4 +1,5 @@
-from day_3 import day3, populate_table, check_diag
+from day_3 import day3, populate_table, check_diag, day3_asterisk, check_ratio
+import pytest
 
 input: list[str] = open("day-3/test_input.txt", "r").readlines()
 input1 = [line.strip("\n") for line in input]
@@ -6,6 +7,17 @@ input1 = [line.strip("\n") for line in input]
 input2 = populate_table(input1)
 
 
-assert (check_diag(0, 2, 0, input2)) == True
+def test_check_diag() -> None:
+    assert (check_diag(0, 2, 0, input2)) == True
 
-assert day3(input2) == 4361
+
+def test_day3() -> None:
+    assert day3(input2) == 4361
+
+
+def test_check_digs() -> None:
+    assert check_ratio(1, 3, input2) == 16345
+
+
+def test_part2() -> None:
+    assert day3_asterisk(input2) == 467835
