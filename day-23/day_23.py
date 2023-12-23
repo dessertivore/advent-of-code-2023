34d8c7a48887fb73e87f015da7d3da908e3be38a
dessertivore <landau.sophie@gmail.com>
2023-12-23 22:37:18 +0000

    Have another stab at Day 23

--- day-23/day_23.py
+++ day-23/day_23.py
@@ -44,8 +44,7 @@ class Graph:
                     neighbours_set.add((x_coord, y_coord + 1))
                 if y_coord > 0 and grid[(x_coord, y_coord - 1)] != "#":
                     neighbours_set.add((x_coord, y_coord - 1))
-            if x_coord == self.max_x and y_coord == self.max_y - 1:
-                neighbours_set.add((0, 1))
+
             self.neighbours[(x_coord, y_coord)] = neighbours_set
 
 
@@ -56,49 +55,102 @@ def find_start(grid: Graph, start_node="S"):
     pass
 
 
-# Function to traverse the DAG
-# and apply Dynamic Programming
-# to find the longest path
-def dfs(
-    grid: Graph, start_node: tuple, dp: dict, visited: set, visited_order: list = []
-):
-    # Mark as visited
-    visited.add(start_node)
-    visited_order.append(start_node)
-    # Traverse for all its children
-    for neighbour in grid.neighbours[start_node]:
-        # If not visited
-        if neighbour not in visited:
-            dfs(grid, neighbour, dp, visited, visited_order)
+# # Function to traverse the DAG
+# # and apply Dynamic Programming
+# # to find the longest path
+# def dfs(
+#     grid: Graph, start_node: tuple, dp: dict, visited: set, visited_order: list = []
+# ):
+#     # Mark as visited
+#     visited.add(start_node)
+#     visited_order.append(start_node)
+#     # Traverse for all its children
+#     for neighbour in grid.neighbours[start_node]:
+#         # If not visited
+#         if neighbour not in visited:
+#             dfs(grid, neighbour, dp, visited, visited_order)
 
-            # Store the max of the paths
-            dp[start_node] = max(dp[start_node], 1 + dp[neighbour])
-            if neighbour == (grid.max_x, grid.max_y - 1):
-                break
+#             # Store the max of the paths
+#             dp[start_node] = max(dp[start_node], 1 + dp[neighbour])
+#             if neighbour == (grid.max_x, grid.max_y - 1):
+#                 break
 
 
-# Function that returns the longest path
-def find_scenic_walk(grid: Graph, start_node: tuple):
+# # Function that returns the longest path
+# def find_scenic_walk(grid: Graph, start_node: tuple):
+#     """
+#     This does not work :( To retry at later date.
+#     """
+#     # Dp stored as dict
+#     dp: dict = defaultdict(lambda: 0)
+#     visited: set = set()
+#     visited_order: list = []
+#     # Visited array to know if the node
+#     # has been visited previously or not
+
+#     # Call DFS for every unvisited vertex
+#     # for i in grid.data.keys():
+#     #     node = i
+#     #     if node not in visited:
+#     dfs(grid, start_node, dp, visited, visited_order)
+
+#     ans = 0
+
+#     # Traverse and find the maximum of all dp[i]
+#     for i in dp.keys():
+#         ans = max(ans, dp[i])
+
+#     return ans, len(visited)
+
+
+def find_scenic_walk(grid, end_node, start_node=(0, 1), previous_node=None):
     """
-    This does not work :( To retry at later date.
+    This uses recursion and it worked but it recursed past my recursion limit with real input :(
+    Trying to reduce use of recursion by grouping together nodes with 1 neighbour.
     """
-    # Dp stored as dict
-    dp: dict = defaultdict(lambda: 0)
-    visited: set = set()
-    visited_order: list = []
-    # Visited array to know if the node
-    # has been visited previously or not
+    if start_node == end_node:  # base case
+        return 0
+    distance = 0
+    counter = 1
+    current_node = start_node
+    while current_node != end_node:
+        if (
+            len(grid.neighbours[current_node]) == 1
+        ):  # while there is only 1 neighbour, no need for recursion
+            counter += 1
+            previous_node = current_node
+            for neighbour in grid.neighbours[current_node]:
+                current_node = neighbour
 
-    # Call DFS for every unvisited vertex
-    # for i in grid.data.keys():
-    #     node = i
-    #     if node not in visited:
-    dfs(grid, start_node, dp, visited, visited_order)
+        elif (
+            len(grid.neighbours[current_node]) == 2
+            and previous_node in grid.neighbours[current_node]
+        ):
+            counter += 1
 
-    ans = 0
+            for neighbour in grid.neighbours[current_node]:
+                if neighbour != previous_node:
+                    next = neighbour
+            previous_node = current_node
+            current_node = next
 
-    # Traverse and find the maximum of all dp[i]
-    for i in dp.keys():
-        ans = max(ans, dp[i])
+        else:
+            break
 
-    return ans, len(visited)
+    if current_node == end_node:
+        return counter
+
+    for neighbour in grid.neighbours[current_node]:
+        print((grid.neighbours[current_node]), previous_node, current_node)
+        if neighbour != previous_node:
+            distance = max(
+                distance,
+                counter + find_scenic_walk(grid, end_node, neighbour, current_node),
+            )
+
+    return distance
+
+
+# map_23 = create_array("day-23/input.txt")
+# graph_map = Graph(map_23)
+# print(find_scenic_walk(graph_map, (graph_map.max_x, graph_map.max_y - 1)))
--- day-23/day_23_tests.py
+++ day-23/day_23_tests.py
@@ -4,6 +4,11 @@ from day_23 import find_start, find_scenic_walk, Graph
 test_map: dict = create_array("day-23/test_input.txt")
 test_map_graph: Graph = Graph(test_map)
 start = find_start(test_map_graph, ".")
-print(start)
-print(find_scenic_walk(test_map_graph, start))
+# print(start)
+# print(find_scenic_walk(test_map_graph, start))
 # assert find_scenic_walk(test_map_graph, start) == 94
+
+assert (
+    find_scenic_walk(test_map_graph, (test_map_graph.max_x, test_map_graph.max_y - 1))
+    == 94
+)
